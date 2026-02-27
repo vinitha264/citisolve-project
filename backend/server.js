d521a82eda320d19ea0c5e64e72678abafd49d80
Vinitha <vinithabairagoni@gmail.com>
2026-02-27 19:55:25 +0530

    Refactor server.js for improved structure

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors({
     origin: "http://localhost:5173", // React frontend
     credentials: true
@@ -15,21 +14,17 @@ app.use(cors({
 
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
 
-// Routes
 const complaintRoutes = require("./routes/complaintRoutes");
 app.use("/complaints", complaintRoutes);
 
-// Test route
 app.get("/", (req, res) => {
     res.send("Server is running...");
 });
 
-// Start server
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
